55102ff96aadf04972272741d248db56c39157f9
wmcphillips708 <wmcphillips708@gmail.com>
2025-05-16 14:33:23 -0400

    Add files via upload

---
+++ QuoteAndBookingPage.jsx
@@ -0,0 +1,6 @@
+import { useState, useRef, useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+// ... truncated for brevity (use full JSX in practice)
+export default function QuoteAndBookingPage() {
+  // component code
+}
---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QuoteAndBookingPage from './QuoteAndBookingPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuoteAndBookingPage />
+  </React.StrictMode>,
+)
