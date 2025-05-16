71b05b445348942142adcec3a1c9821dca3730b0
wmcphillips708 <wmcphillips708@gmail.com>
2025-05-16 15:35:07 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import QuoteAndBookingPage from './QuoteAndBookingPage.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QuoteAndBookingPage />
-  </React.StrictMode>,
-)
