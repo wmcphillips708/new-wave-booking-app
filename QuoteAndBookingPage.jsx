c4c2946f3486e98024267bb1a7f0d97678fc8d14
wmcphillips708 <wmcphillips708@gmail.com>
2025-05-16 15:35:25 -0400

    Delete QuoteAndBookingPage.jsx

--- QuoteAndBookingPage.jsx
+++
@@ -1,6 +0,0 @@
-import { useState, useRef, useEffect } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-// ... truncated for brevity (use full JSX in practice)
-export default function QuoteAndBookingPage() {
-  // component code
-}
